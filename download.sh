8a856bc8eb66e812074e9f60cd87e06efda46da0
YuanzhiZhu <yuazhu@student.ethz.ch>
2023-09-14 18:31:02 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -4,7 +4,7 @@ URL_1="https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_dif
 OUTPUT_FILE_1="model_zoo/256x256_diffusion_uncond.pt"
 wget $URL_1 -O $OUTPUT_FILE_1
 
-pip install gdown
-FILE_ID="1jElnRoFv7b31fG0v6pTSQkelbSX3xGZh"
+pip install gdown==4.7.1
+FILE_ID="1BGwhRWUoguF-D8wlZ65tf227gp3cDUDh"
 OUTPUT_FILE_2="model_zoo/diffusion_ffhq_10m.pt"
 gdown --id $FILE_ID -O $OUTPUT_FILE_2
